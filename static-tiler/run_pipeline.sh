76199990907aa3f61f00dded853422aa8cae4b88
Fried_Squid <ace@danucore.com>
2026-02-16 14:41:10 +0000

    test

--- static-tiler/run_pipeline.sh
+++ static-tiler/run_pipeline.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
 
-echo "Starting GStreamer pipeline with RTP loopback sources..."
+echo "Starting GStreamer pipeline with raw RTP sources..."
 
 gst-launch-1.0 \
   nvstreammux name=mux width=1920 height=1080 batch-size=16 batched-push-timeout=40000 ! \
@@ -9,19 +8,15 @@ gst-launch-1.0 \
   h264parse config-interval=-1 ! \
   mpegtsmux ! \
   tcpserversink host=0.0.0.0 port=6000 sync=false \
-  udpsrc port=2001 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_0 \
-  udpsrc port=2002 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_1 \
-  udpsrc port=2003 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_2 \
-  udpsrc port=2004 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_3 \
-  udpsrc port=2005 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_4 \
-  udpsrc port=2006 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_5 \
-  udpsrc port=2007 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_6 \
-  udpsrc port=2008 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_7 \
-  udpsrc port=2009 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_8 \
-  udpsrc port=2010 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_9 \
-  udpsrc port=2011 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_10 \
-  udpsrc port=2012 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_11 \
-  udpsrc port=2013 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_12 \
-  udpsrc port=2014 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_13 \
-  udpsrc port=2015 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_14 \
-  udpsrc port=2016 ! application/x-rtp,encoding-name=H264 ! rtph264depay ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_15
+  udpsrc port=2001 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=X-GST ! rtpgstdepay ! "video/x-raw,width=1920,height=1080,framerate=60/1" ! queue ! videoconvert ! "video/x-raw,format=RGBA" ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_0 \
+  udpsrc port=2002 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=X-GST ! rtpgstdepay ! "video/x-raw,width=1920,height=1080,framerate=60/1" ! queue ! videoconvert ! "video/x-raw,format=RGBA" ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_1 \
+  udpsrc port=2003 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=X-GST ! rtpgstdepay ! "video/x-raw,width=1920,height=1080,framerate=60/1" ! queue ! videoconvert ! "video/x-raw,format=RGBA" ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_2 \
+  udpsrc port=2004 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=X-GST ! rtpgstdepay ! "video/x-raw,width=1920,height=1080,framerate=60/1" ! queue ! videoconvert ! "video/x-raw,format=RGBA" ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_3 \
+  udpsrc port=2005 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=X-GST ! rtpgstdepay ! "video/x-raw,width=1920,height=1080,framerate=60/1" ! queue ! videoconvert ! "video/x-raw,format=RGBA" ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_4 \
+  udpsrc port=2006 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=X-GST ! rtpgstdepay ! "video/x-raw,width=1920,height=1080,framerate=60/1" ! queue ! videoconvert ! "video/x-raw,format=RGBA" ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_5 \
+  udpsrc port=2007 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=X-GST ! rtpgstdepay ! "video/x-raw,width=1920,height=1080,framerate=60/1" ! queue ! videoconvert ! "video/x-raw,format=RGBA" ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_6 \
+  udpsrc port=2008 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=X-GST ! rtpgstdepay ! "video/x-raw,width=1920,height=1080,framerate=60/1" ! queue ! videoconvert ! "video/x-raw,format=RGBA" ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_7 \
+  udpsrc port=2009 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=X-GST ! rtpgstdepay ! "video/x-raw,width=1920,height=1080,framerate=60/1" ! queue ! videoconvert ! "video/x-raw,format=RGBA" ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_8 \
+  udpsrc port=2010 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=X-GST ! rtpgstdepay ! "video/x-raw,width=1920,height=1080,framerate=60/1" ! queue ! videoconvert ! "video/x-raw,format=RGBA" ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_9 \
+  udpsrc port=2011 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=X-GST ! rtpgstdepay ! "video/x-raw,width=1920,height=1080,framerate=60/1" ! queue ! videoconvert ! "video/x-raw,format=RGBA" ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_10 \
+  udpsrc port=2012 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=X-GST ! rtpgstdepay ! "video/x-raw,width=1920,height=1080,framerate=60/1" ! queue ! videoconvert ! "video/x-raw,format=RGBA" ! nvvideoconvert ! "video/x-raw(memory:NVMM),format=NV12" ! mux.sink_11
